7b87c275d710f4780a74846e3f28e2fd0d8ed3f8
Rub28 <ruhernandezm@gmail.com>
2024-10-31 16:23:21 -0600

    :#

---
+++ src/main.jsx
@@ -0,0 +1,15 @@
+//import react from '@vitejs/plugin-react-swc'
+import React from  'react'
+import ReactDOM from 'react-dom/client'  
+import {App} from './App.jsx'
+import './index.css'
+
+
+const root =  ReactDOM.createRoot(document.getElementById('root')) 
+
+root.render (   
+     <App/>   
+ )  
+    
+
+
